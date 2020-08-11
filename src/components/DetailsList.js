c9ca69fed9d4d8ef89ea1c742f2d9e12db767766
Nathalia Bruno <nathi.bruno@gmail.com>
2020-08-11 02:32:51 -0300

    add alt on images

--- src/components/DetailsList.js
+++ src/components/DetailsList.js
@@ -11,7 +11,7 @@ function DetailsList(props) {
   for (let i = 0; i < list.length; i++) {
     list[i] && listItems.push(
       <li key={`${list[i].id}`} className="details-content-section-list-item">
-        <img className="details-content-section-list-item-image" src={generateThumbURL(list[i].thumbnail)} />
+        <img className="details-content-section-list-item-image" src={generateThumbURL(list[i].thumbnail)} alt={list[i].title}/>
         <h5 className="details-content-section-list-item-title">{list[i].title}</h5>
       </li>)
   }
